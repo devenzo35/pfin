2e23cac049b6518bf2b563c56f7e5caafc71a957
Enzo <enzocuellar12@gmail.com>
2024-06-20 09:36:09 -0300

    authentication added, still working on it

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import MyView, HomePageView
+from core.views import RegisterView, HomePageView
 
 
 urlpatterns = [
-    path("home", HomePageView.as_view(), name="home")
+    path("home", HomePageView.as_view(), name="home"),
+    path("register", RegisterView, name="home")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView  
+from django.contrib.auth.models import User  
+from .forms import CreateUserForm
+
 # Create your views here.
 
 class MyView(View):
@@ -11,4 +14,24 @@ class MyView(View):
     
 class HomePageView(TemplateView):
      template_name="home.html"
+     
+class LoginView():
+    template_name = "./templates/auth/user_form.html"
+   
+    
+def RegisterView(request):
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            user= User.objects.create_user(username,email,password) 
+            
+            return redirect("home")
+    else:
+        form = CreateUserForm()
+    
+    return render(request, "registration/register.html", {'form': form})
      
